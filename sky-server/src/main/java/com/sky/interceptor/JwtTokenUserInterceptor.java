179e538b27ad4283c5cbe65324d9bc9ca975ff3f
user lv <2716570779@qq.com>
2024-05-07 15:13:09 +0800

    缓存套餐代码开发

--- sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
+++ sky-server/src/main/java/com/sky/interceptor/JwtTokenUserInterceptor.java
@@ -53,7 +53,7 @@ public class JwtTokenUserInterceptor implements HandlerInterceptor {
             Claims claims = JwtUtil.parseJWT(jwtProperties.getUserSecretKey(), token);
             Long userid = Long.valueOf(claims.get(JwtClaimsConstant.USER_ID).toString());
             BaseContext.setCurrentId(userid);
-            log.info("当前用户id：", userid);
+            log.info("当前用户id：{}", userid);
             //3、通过，放行
             return true;
         } catch (Exception ex) {
