46a9fc2e085fe6e664aef802e20eee5ab62c6923
user lv <2716570779@qq.com>
2024-05-09 22:09:08 +0800

    工作台业务代码开发

--- sky-server/src/main/java/com/sky/mapper/OrderMapper.java
+++ sky-server/src/main/java/com/sky/mapper/OrderMapper.java
@@ -1,6 +1,7 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.dto.GoodsSalesDTO;
 import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.entity.Orders;
 import com.sky.entity.ShoppingCart;
@@ -10,6 +11,7 @@ import org.apache.ibatis.annotations.Update;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Map;
 
 @Mapper
 public interface OrderMapper {
@@ -75,4 +77,9 @@ public interface OrderMapper {
     @Select("select * from orders where status = #{status} and order_time < #{orderTime};")
     List<Orders> getByStatusAndOrderTimeLT(Integer status,LocalDateTime orderTime);
 
+    Double sumByMap(Map map);
+
+    Integer countByMap(Map map);
+
+    List<GoodsSalesDTO> getSalesTop10(LocalDateTime begin, LocalDateTime end);
 }
